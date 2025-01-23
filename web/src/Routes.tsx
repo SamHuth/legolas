9a8d9def26451ac05c4f22061dd841882739ca62
SamuelHuth <sams.huth@gmail.com>
2025-01-23 20:12:05 +1000

    dbAuth and login

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,25 +1,15 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
+import { Router, Route } from '@redwoodjs/router'
 
-import { Set, Router, Route } from '@redwoodjs/router'
-
-import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
+import { useAuth } from './auth'
 
 const Routes = () => {
   return (
-    <Router>
-      <Set wrap={ScaffoldLayout} title="Countries" titleTo="countries" buttonLabel="New Country" buttonTo="newCountry">
-        <Route path="/countries/new" page={CountryNewCountryPage} name="newCountry" />
-        <Route path="/countries/{id:Int}/edit" page={CountryEditCountryPage} name="editCountry" />
-        <Route path="/countries/{id:Int}" page={CountryCountryPage} name="country" />
-        <Route path="/countries" page={CountryCountriesPage} name="countries" />
-      </Set>
+    <Router useAuth={useAuth}>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+      <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- web/src/pages/Country/CountriesPage/CountriesPage.tsx
+++
@@ -1,7 +0,0 @@
-import CountriesCell from 'src/components/Country/CountriesCell'
-
-const CountriesPage = () => {
-  return <CountriesCell />
-}
-
-export default CountriesPage
